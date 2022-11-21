41d3aa8361ac84263249d164fb46bda46d4a727f
Florian <florian.lopitaux@etu.univ-amu.fr>
2022-11-21 10:42:20 +0100

    Add functionnality of message error to start button

--- src/main/java/fr/projectGroup/appkana/HomePageController.java
+++ src/main/java/fr/projectGroup/appkana/HomePageController.java
@@ -2,6 +2,7 @@ package fr.projectGroup.appkana;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 
 public class HomePageController {
     // FIELDS
@@ -14,10 +15,22 @@ public class HomePageController {
     @FXML
     private CheckBox dakutenCheckBox;
 
+    @FXML
+    private Label errorButtonMessage;
+
 
     // METHODS
     @FXML
     protected void onStartGameButtonClick() {
         System.out.println("click on button !");
+
+        if (!hiraganaCheckBox.isSelected() && !katakanaCheckBox.isSelected()) {
+            errorButtonMessage.setText("You need to select at least hiragana or katakana characters");
+            return;
+        } else {
+            errorButtonMessage.setText("");
+        }
+
+        System.out.println("Launch game !");
     }
 }
