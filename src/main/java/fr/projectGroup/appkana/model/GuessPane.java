620e5a1219c5da8415a4f9e11042ebbb7c9dd494
ReNathan <nathan.reboul@etu.univ-amu.fr>
2022-11-28 15:26:45 +0100

    Add hiragana image files and styling guess pane

--- src/main/java/fr/projectGroup/appkana/model/GuessPane.java
+++ src/main/java/fr/projectGroup/appkana/model/GuessPane.java
@@ -1,12 +1,10 @@
 package fr.projectGroup.appkana.model;
 
 import fr.projectGroup.appkana.controller.GamePageController;
-import javafx.beans.property.IntegerProperty;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.VBox;
 
-import java.util.Set;
 
 public class GuessPane extends VBox {
     // FIELDS
@@ -17,7 +15,6 @@ public class GuessPane extends VBox {
     // CONSTRUCTOR
     public GuessPane(Kana kana, GamePageController gamePageController) {
         this.setId("GuessPane");
-
         this.kana = kana;
 
         this.buildWidget(gamePageController);
@@ -42,8 +39,10 @@ public class GuessPane extends VBox {
 
             if (kana.getSyllable().getRomanji().equalsIgnoreCase(this.textField.getText())) {
                 gamePageController.getPlayerScore().setValue(gamePageController.getPlayerScore().get() + 1);
+                this.textField.setStyle("-fx-background-color: #06f606; -fx-highlight-text-fill: white");
+
             } else {
-                System.out.println("Mauvaise syllabe");
+                this.textField.setStyle("-fx-background-color: red; -fx-text-fill: white");
             }
 
             this.textField.setDisable(true);
