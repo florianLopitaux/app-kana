95df2c2923e50d5e8e2eac1d32c0f2d5a6da0240
ReNathan <nathan.reboul@etu.univ-amu.fr>
2022-11-28 09:58:53 +0100

    Beginning of generic pane to guess a kana

---
+++ src/main/java/fr/projectGroup/appkana/model/GuessPane.java
@@ -0,0 +1,37 @@
+package fr.projectGroup.appkana.model;
+
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.VBox;
+
+public class GuessPane extends VBox {
+    // FIELDS
+    private final Kana kana;
+    private TextField textField;
+
+
+    // CONSTRUCTOR
+    public GuessPane(Kana kana) {
+        this.kana = kana;
+        this.buildWidget();
+    }
+
+
+    // METHODS
+    private void buildWidget() {
+        this.getChildren().add(this.kana.getImage());
+
+        this.textField = new TextField();
+        this.textField.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                if (kana.getSyllable().getRomanji().equalsIgnoreCase(this.textField.getText())) {
+                    System.out.println("bien joué");
+                } else {
+                    System.out.println("pas bien joué");
+                }
+            }
+        });
+
+        this.getChildren().add(this.textField);
+    }
+}
