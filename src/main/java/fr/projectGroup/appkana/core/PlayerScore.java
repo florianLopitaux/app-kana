8e7e34d099258c99964def5b1cf020b8829dfc7f
florianLopitaux <florian.lopitaux@etu.univ-amu.fr>
2022-12-14 14:12:45 +0100

    Convert playerScore class to a record

--- src/main/java/fr/projectGroup/appkana/core/PlayerScore.java
+++ src/main/java/fr/projectGroup/appkana/core/PlayerScore.java
@@ -2,37 +2,14 @@ package fr.projectGroup.appkana.core;
 
 import java.util.Objects;
 
-public class PlayerScore {
-    // FIELDS
-    private final String name;
-    private final double score;
-
-
-    // CONSTRUCTOR
-    public PlayerScore(String name, double score) {
-        this.name = name;
-        this.score = score;
-    }
-
-
-    // GETTERS
-    public String getName() {
-        return this.name;
-    }
-
-    public double getScore() {
-        return this.score;
-    }
-
+/**
+ * @param name FIELDS
+ */
+public record PlayerScore(String name, double score) {
     // METHODS
     @Override
     public String toString() {
         return "PlayerScore[name=" + this.name + ", score=" + this.score + "]";
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, score);
     }
 
     @Override
