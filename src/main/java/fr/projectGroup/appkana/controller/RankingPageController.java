ee597d429e9d4ecef6a2b2a2ee9d2c56954b7451
florianLopitaux <florian.lopitaux@etu.univ-amu.fr>
2022-12-14 15:19:32 +0100

    Build file to the Ranking page (fxml, controller, css)

---
+++ src/main/java/fr/projectGroup/appkana/controller/RankingPageController.java
@@ -0,0 +1,25 @@
+package fr.projectGroup.appkana.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class RankingPageController extends VBox implements JavaFXControllable {
+    // FIELDS
+    Stage primaryStage;
+
+
+    // CONSTRUCTOR
+    public RankingPageController(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+
+        this.loadFXMLFile("Ranking");
+    }
+
+
+    // METHODS
+    @FXML
+    private void initialize() {
+
+    }
+}
