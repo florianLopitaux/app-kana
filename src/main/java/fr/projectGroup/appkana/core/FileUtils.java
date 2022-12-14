2c8953e82e9a77aa7c025ff6139cd512d0c39e7d
florianLopitaux <florian.lopitaux@etu.univ-amu.fr>
2022-12-14 11:58:00 +0100

    Create files to stock data for the project

---
+++ src/main/java/fr/projectGroup/appkana/core/FileUtils.java
@@ -0,0 +1,13 @@
+package fr.projectGroup.appkana.core;
+
+import java.util.List;
+
+public class FileUtils {
+    public static void registerNewScore() {
+
+    }
+
+    public static List<PlayerScore> readAllScores() {
+
+    }
+}
