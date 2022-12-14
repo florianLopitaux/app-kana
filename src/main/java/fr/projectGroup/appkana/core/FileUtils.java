543a3ab81d95949f3c4a53e115c75821a1e20504
florianLopitaux <florian.lopitaux@etu.univ-amu.fr>
2022-12-14 13:30:00 +0100

    code FileUtils class to register score of the app

--- src/main/java/fr/projectGroup/appkana/core/FileUtils.java
+++ src/main/java/fr/projectGroup/appkana/core/FileUtils.java
@@ -1,13 +1,32 @@
 package fr.projectGroup.appkana.core;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileUtils {
-    public static void registerNewScore() {
+    public static void registerNewScore(PlayerScore newScore) throws IOException {
+        final PrintWriter writer = new PrintWriter(new FileWriter("ScoresFile.txt"));
 
+        writer.println("");
+        writer.println(newScore.getName() + " " + newScore.getScore());
+
+        writer.close();
     }
 
-    public static List<PlayerScore> readAllScores() {
+    public static List<PlayerScore> readAllScores() throws IOException {
+        final List<PlayerScore> playersScore = new ArrayList<>();
+        final BufferedReader reader = new BufferedReader(new FileReader("ScoresFile.txt"));
+        String line;
 
+        while ((line = reader.readLine()) != null) {
+            final String name = line.split(" ")[0];
+            final float points = Float.parseFloat(line.split(" ")[1]);
+
+            playersScore.add(new PlayerScore(name, points));
+        }
+
+        reader.close();
+        return playersScore;
     }
 }
